66f6ae8b530628e2d701433356cc9f592e857177
m-mueller678 <marcus.mueller.678@gmail.com>
2017-02-18 20:59:18 +0100

    fixed projectile collision

--- common/src/game/projectile.rs
+++ common/src/game/projectile.rs
@@ -14,12 +14,28 @@ pub struct Projectile {
     sprite: graphics::Sprite,
 }
 
+fn dot_p(v1: &[i32; 2], v2: &[i32; 2]) -> i32 {
+    v1[0] * v2[0] + v1[1] * v2[1]
+}
+
+fn cross_p_scalar(v1: &[i64; 2], v2: &[i64; 2]) -> i64 {
+    v1[0] * v2[1] - v1[1] * v2[0]
+}
+
+fn collides(target: &[i64; 2], path: &[i64; 2], radius: i64) -> bool {
+    let move_len_sq = path[0] * path[0] + path[1] * path[1];
+    //area of parallelogram defined by points: start, end, target
+    let move_area = cross_p_scalar(target, path) as i64;
+    let min_dist_sq = move_area.saturating_mul(move_area) / move_len_sq;
+    min_dist_sq <= radius * radius
+}
+
 impl Projectile {
     pub fn new(target: Rc<RefCell<Ship>>, x: i32, y: i32, v: i32, dmg: Damage, sprite: graphics::Sprite) -> Self {
         let weak = Rc::downgrade(&target);
         let target = target.borrow();
-        let dx = target.pos_x();
-        let dy = target.pos_y();
+        let dx = target.pos_x() - x;
+        let dy = target.pos_y() - y;
         let hyp = max((dx as f64).hypot(dy as f64) as i32, 1);
         Projectile {
             target: weak,
@@ -32,24 +48,24 @@ impl Projectile {
         }
     }
     pub fn tick(&mut self, game_size_x: i32, game_size_y: i32) -> bool {
+        self.pos_x += self.v_x;
+        self.pos_y += self.v_y;
         if let Some(target) = Weak::upgrade(&self.target) {
             let mut target = target.borrow_mut();
             let dx = target.pos_x() - self.pos_x;
             let dy = target.pos_y() - self.pos_y;
-            if dx * dx + dy * dy <= 100 {
-                target.apply_damage(&self.dmg);
-                false
-            } else {
-                self.pos_x += self.v_x;
-                self.pos_y += self.v_y;
-                if dx * self.v_x + dy * self.v_y < 0 {
+            if dot_p(&[dx, dy], &[self.v_x, self.v_y]) < 0 {
+                if collides(&[dx as i64, dy as i64], &[-self.v_x as i64, -self.v_y as i64], 20) {
+                    target.apply_damage(&self.dmg);
+                    false
+                } else {
                     self.target = Weak::new();
+                    true
                 }
+            } else {
                 true
             }
         } else {
-            self.pos_x += self.v_x;
-            self.pos_y += self.v_y;
             self.pos_x >= 0 && self.pos_y >= 0 && self.pos_x < game_size_x && self.pos_y < game_size_y
         }
     }
